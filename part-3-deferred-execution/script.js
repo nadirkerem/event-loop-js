0634ef0b4a9448d1ab2fe2eda4714e44c98a333a
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-06-13 12:16:37 -0400

    complete part 3 deferred execution

---
+++ part-3-deferred-execution/script.js
@@ -0,0 +1,61 @@
+/*
+const text = document.createElement('p');
+
+
+function primeUpToN(n) {
+  let primeNumbers = [];
+  for (let i = 3; i < n; i += 2) {
+    let isPrime = true;
+    for (let m = 3; m ** 2 <= i; m += 2) {
+      if (i % m === 0) {
+        isPrime = false;
+      }
+    }
+    if (isPrime) {
+      primeNumbers.push(i);
+    }
+  }
+
+  text.innerHTML = primeNumbers.join('<br>');
+  document.body.appendChild(text);
+  alert('The calculation is finished');
+}
+
+primeUpToN(10000);
+*/
+
+const text = document.createElement('p');
+document.body.appendChild(text);
+
+function primeUpToN(n) {
+  let primeNumbers = [];
+  let i = 3;
+
+  function calculateNextPrime() {
+    if (i >= n) {
+      text.innerHTML = primeNumbers.join('<br>');
+      alert('The calculation is finished');
+      return;
+    }
+
+    let isPrime = true;
+    for (let m = 3; m ** 2 <= i; m += 2) {
+      if (i % m === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      primeNumbers.push(i);
+      text.innerHTML = primeNumbers.join('<br>');
+    }
+
+    i += 2;
+    setTimeout(calculateNextPrime, 0);
+  }
+
+  calculateNextPrime();
+}
+
+primeUpToN(10000);
